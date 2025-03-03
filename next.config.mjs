55d504b1de0ba8765060732112b0060509aaa23e
村田さとし <muratasatoshi@MacBook-Pro.local>
2025-03-04 02:08:33 +0900

    Fix: Next.js 設定修正 & Vercel デプロイ対応

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    experimental: {
-      appDir: true,
-    },
-    trailingSlash: true, // 追加
+    trailingSlash: true,
+    output: "export", // `next export` に対応する設定
   };
   
   export default nextConfig;
